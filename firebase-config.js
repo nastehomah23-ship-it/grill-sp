e951f2163fb1deaf6d659243f4dda61fc100fe38
nastehomah23-ship-it <nasteho.mah23@gmail.com>
2026-05-14 17:33:19 +0200

    Update Firebase config with actual credentials

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBXjjGK-cAkKC4CSOUyxYa23BP7TYOmTkw",
+  authDomain: "grill-spel.firebaseapp.com",
+  projectId: "grill-spel",
+  storageBucket: "grill-spel.firebasestorage.app",
+  messagingSenderId: "558218357374",
+  appId: "1:558218357374:web:eeaef6d629c6da47a0062f",
+  measurementId: "G-JGB0GQ0R81"
 };
 
 export { firebaseConfig };
